ce3cc6400c2a24355ca8d694a9a9d2d74f9d63aa
meowscules <79730328+meowscules@users.noreply.github.com>
2021-10-14 13:48:55 +0300

    ver 1.1
    redef == (eq) and +(add)

--- matrix.py
+++ matrix.py
@@ -1,12 +1,4 @@
 import random
-def printMatrix(object):
-    for i in range(len(object.matrix)):
-        for j in range(len(object.matrix[i])):
-            print(object.matrix[i][j], end=' ')
-        print()
-    print()
-
-
 
 class Matrix: #m - строки n - столбцы
     def __init__(self, M, N):
@@ -20,12 +12,18 @@ class Matrix: #m - строки n - столбцы
                 self.matrix[i][j] = random.randint(0, 1000)
 
     def __add__(self, other):
-            if (len(self.matrix) == len(other.matrix) and len(self.matrix[0]) == len(other.matrix[0])):
-                for i in range(len(self.matrix)):
-                    for j in range(len(self.matrix[i])):
-                        self.matrix[i][j] += other.matrix[i][j]
-            printMatrix(object1)
-            else:
-                print("Матрицы не равны")
+        matrixSum = Matrix(len(self.matrix), len(self.matrix[0]))
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                matrixSum.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
+        return matrixSum.matrix
 
-    def matrixSum():
+    def __eq__(self, other):
+       return (len(self.matrix) == len(other.matrix) and len(self.matrix[0]) == len(other.matrix[0]))
+
+    def printMatrix(self):
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                print(self.matrix[i][j], end=' ')
+            print()
+        print()
